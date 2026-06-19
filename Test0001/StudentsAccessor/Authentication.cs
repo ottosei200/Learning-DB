1020238b8d39a9a3ebdd2a1ae6de53ed1fccbeec
ottosei200 <shotaro.sharo.8112@gmail.com>
2026-06-19 22:07:54 +0900

    超基本的なDB接続
    MySQLにvscode_userていうアカウントでログイン

---
+++ Test0001/StudentsAccessor/Authentication.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsAccessor {
+    public class Authentication {
+        public static string ConsoleInputPassword() {
+            string password = "";
+
+            // マスキング入力
+            while (true) {
+                var key = Console.ReadKey(intercept: true);
+                if (key.Key == ConsoleKey.Enter) {
+                    Console.WriteLine();
+                    break;
+                } else if (key.Key == ConsoleKey.Backspace && password.Length > 0) {
+                    password = password.Substring(0, password.Length - 1);
+
+                    Console.Write("\b \b");
+                } else if (!char.IsControl(key.KeyChar)) {
+                    password += key.KeyChar;
+                    Console.Write('*');
+                }
+            }
+
+            return password;
+        }
+    }
+}
---
+++ Test0001/StudentsAccessor/Program.cs
@@ -0,0 +1,32 @@
+﻿using MySqlConnector;
+
+namespace StudentsAccessor {
+    internal class Program {
+        static void Main(string[] args) {
+            // ログイン
+            Console.Write("Enter the password: ");
+            string password = Authentication.ConsoleInputPassword();
+            Console.WriteLine("Logging in ...");
+
+            // ロックされたときは'sudo mysqladmin flush-hosts'でログイン可能に戻す
+            string connectionString = $"Server=localhost;Database=practice0001;Uid=vscode_user;Pwd={password}";
+
+            using (var connection = new MySqlConnection(connectionString)) {
+                try {
+                    connection.Open();
+                    Console.WriteLine("MySQLサーバーに接続しました。");
+
+                    string sql = "select version(); ";
+                    using (var command = new MySqlCommand(sql, connection)) {
+                        var version = command.ExecuteScalar();
+                        Console.WriteLine($"MySQLのバージョン: {version}");
+                    }
+                } catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                } finally {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
