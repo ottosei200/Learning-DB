6cc965c22299486453fa6175722a593eb65cbd3c
ottosei200 <shotaro.sharo.8112@gmail.com>
2026-06-20 04:18:26 +0900

    envにstudents.sql追加

---
+++ env/students.sql
@@ -0,0 +1,15 @@
+mysql: [Warning] Using a password on the command line interface can be insecure.
+Welcome to the MySQL monitor.  Commands end with ; or \g.
+Your MySQL connection id is 15
+Server version: 9.7.1 MySQL Community Server - GPL
+
+Copyright (c) 2000, 2026, Oracle and/or its affiliates.
+
+Oracle is a registered trademark of Oracle Corporation and/or its
+affiliates. Other names may be trademarks of their respective
+owners.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+mysql> ^C
+mysql> 
